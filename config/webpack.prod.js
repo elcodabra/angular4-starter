0f1ed7fef61410580c18c9c66e88c4ff5884fcc1
Alex Konovalov <akonovalov@at-consulting.ru>
2017-09-20 16:36:20 +0300

    update prod

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -9,7 +9,7 @@ const commonConfig = require('./webpack.common.js');
 const ENV = process.env.NODE_ENV = process.env.ENV = 'production';
 
 module.exports = webpackMerge(commonConfig, {
-  devtool: 'eval',
+  devtool: '',
 
   entry: {
     main: [
